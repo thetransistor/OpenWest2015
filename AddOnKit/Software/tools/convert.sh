502f10b2eacc9b6ee6106e09c262862f7f086243
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-05-09 16:42:00 -0400

    General cleanups and enhancements.
    I've removed bashisms and converted the script to a zsh script. I've
    added arguments so users can specify the input and output files. I
    added sanity checking around the input file.

--- AddOnKit/Software/tools/convert.sh
+++ AddOnKit/Software/tools/convert.sh
@@ -1,8 +1,40 @@
-#!/bin/bash
-convert $1 -resize 320x240 -gravity center -background black \
+#!/usr/bin/env zsh
+
+usage() {
+	echo "USAGE: ${0} [-i /path/to/input/file] [-o /path/to/output/file]"
+	exit 1
+}
+
+input=""
+output=""
+
+while getopts "i:o:h?" o; do
+	case "${o}" in
+	i)
+		input="${OPTARG}"
+		;;
+	o)
+		output="${OPTARG}"
+		;;
+	*)
+		usage
+		;;
+	esac
+done
+
+if [ "${input}" = "" ] || [ "${output}" = "" ]; then
+	usage
+fi
+
+if [ ! -f "${input}" ]; then
+	echo "[-] File not found: ${file}"
+	exit 1
+fi
+
+convert "${input}" -resize 320x240 -gravity center -background black \
     -rotate 180 -extent 320x240 \
     -depth 24 \
-    bmp3:1.bmp
+    "${output}"
     # -type truecolor \
     # -set colorspace sRGB -colorspace RGB \
     # -colorspace RGB \
