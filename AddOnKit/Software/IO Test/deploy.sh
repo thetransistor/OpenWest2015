18b148a4fb8c76a4475a05d860aa19db2d6437ec
Joe Skeen <joe.skeen@imail.org>
2015-05-09 17:34:06 -0600

    Updated deploy script to prompt for volume, and give user more feedback as to what is going on.

--- AddOnKit/Software/IO Test/deploy.sh
+++ AddOnKit/Software/IO Test/deploy.sh
@@ -1,3 +1,12 @@
+echo "Copying ‘io-test.binary’ to ‘run.bin’…"
 cp io-test.binary run.bin
-cp run.bin /Volumes/Untitled/
+VOLUME="$1" 
+if [ -z "$VOLUME" ]
+  then
+    echo "Which volume to copy run.bin to? (WARNING: It will be unmounted after copy.)"
+    ls /Volumes/
+    read VOLUME
+fi
+cp run.bin /Volumes/$VOLUME/
 diskutil unmountdisk /dev/disk2
+echo "You may now safely remove your SD card."
